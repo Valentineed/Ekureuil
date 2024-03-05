55359105fb7536787bc01ab14e17aaf2217626be
Valentin Edouard Emile Dol <49204480+Valentineed@users.noreply.github.com>
2024-03-05 10:05:02 -0500

    add old project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform m_target;
+
+    public Vector3 m_offset;
+
+    public bool m_useOffsetValues;
+
+    public float m_rotationSpeed;
+
+    public Transform m_pivot;
+
+    public float m_maxViewAngle = 65f;
+    public float m_minViewAngle = -60f;
+
+    public bool m_invertY;
+
+    private float m_translateY;
+    private float m_waveSlice;
+    private float m_timer = 0f;
+    public float m_bobbingSpeed = 0.18f;
+    public float m_bobbingY = 0.2f;
+    public float m_midPoint = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!m_useOffsetValues)
+        {
+            m_offset = m_target.position - transform.position;
+        }
+
+        m_pivot.transform.position = m_target.transform.position;
+        m_pivot.transform.parent = m_target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+       
+        //Get the X position of the mouse & rotate the camera
+        float horizontal = Input.GetAxis("Mouse X") * m_rotationSpeed;
+        m_target.Rotate(0, horizontal, 0);
+        //m_pivot.Rotate(0, horizontal, 0);
+
+        //Get the Y position of the mouse & rotate the pivot
+        float vertical = Input.GetAxis("Mouse Y") * m_rotationSpeed;
+        if (m_invertY)
+        {
+            m_pivot.Rotate(vertical, 0, 0);
+        }
+        else
+        {
+            m_pivot.Rotate(-vertical, 0, 0);
+        }
+
+        //Limit up/down camera rotation
+        if (m_pivot.rotation.eulerAngles.x > m_maxViewAngle && m_pivot.rotation.eulerAngles.x < 180f )
+        {
+            m_pivot.rotation = Quaternion.Euler(m_maxViewAngle, 0, 0);
+        }
+
+        if(m_pivot.rotation.eulerAngles.x > 180f && m_pivot.rotation.eulerAngles.x < 360f + m_minViewAngle)
+        {
+            m_pivot.rotation = Quaternion.Euler(360f + m_minViewAngle, 0, 0);
+        }
+
+        //Move the Camera based on the current position on the target & the original offset
+        float yAngle = m_target.eulerAngles.y;
+        float xAngle = m_pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(xAngle, yAngle, 0);
+
+        transform.position = m_target.position - (rotation * m_offset);
+        //transform.position = m_target.position - m_offset;
+
+        m_waveSlice = 0f;
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) == 0 && Mathf.Abs(Input.GetAxis("Vertical")) == 0)
+        {
+            m_timer = 0f;
+        }
+        else
+        {
+            m_waveSlice = Mathf.Sin(m_timer);
+            m_timer = m_timer + m_bobbingSpeed;
+
+            if (m_timer > Mathf.PI * 2)
+            {
+                m_timer = m_timer - (Mathf.PI * 2);
+            }
+        }
+        if (m_waveSlice != 0f)
+        {
+            m_translateY = (m_waveSlice * m_bobbingY) * Mathf.Clamp((Mathf.Abs(Input.GetAxis("Horizontal")) + Mathf.Abs(Input.GetAxis("Vertical"))), 0f, 1f);
+            transform.position = new Vector3(transform.position.x, m_midPoint + m_translateY, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, m_midPoint, transform.position.z);
+        }
+
+        //Stop the angle of camera
+        if (transform.position.y < m_target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, m_target.position.y - 0.5f, transform.position.z);
+        }
+        transform.rotation = rotation;
+        //transform.rotation = Quaternion.Euler(m_pivot.rotation.x, m_target.transform.rotation.y, m_target.transform.rotation.z);
+        //transform.LookAt(m_target);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody m_body;
+
+    public CharacterController m_controller;
+    float m_movSpeed = 25f;
+    public float m_jumpForce = 35f;
+    public float m_gravityScale = 10f;
+    bool m_wallJump = false;
+
+    public float m_distRay;
+    float m_distFront = 0.7f;
+    public float m_airTime = 0f;
+    Vector3 m_dirRayDow;
+    Vector3 m_vecFrontDow;
+    Vector3 m_vecFrontUp;
+    Vector3 m_direction;
+    public float magnitude;
+    public float m_jumpWallForce = 30;
+    int m_Jump = 2;
+
+    //float m_airSpeed = 20f;
+
+
+    void Start()
+    {
+        m_controller = GetComponent<CharacterController>();
+        m_body = GetComponent<Rigidbody>();
+        m_distRay = 1.115f;
+        m_dirRayDow = new Vector3(0, -1, 0);
+
+        m_vecFrontDow = new Vector3(0f, -1f, 0f);
+        m_vecFrontUp = new Vector3(0f, 1f, 0f);
+        m_direction = Vector3.zero;
+    }
+
+    void Update()
+    {
+
+        //DirLook();
+        PlayerMove();
+
+
+
+        if (m_controller.isGrounded)
+        {
+            m_Jump = 2;
+            m_wallJump = false;
+            m_direction.y = 0f;
+          
+        }
+        if (Input.GetButtonDown("Jump") && m_Jump > 0)
+        {
+            m_Jump--;
+            m_direction.y = m_jumpForce;
+        }
+
+
+
+
+
+        //DEBUG
+        Debug.DrawRay(transform.position, m_dirRayDow * m_distRay, Color.red);
+        Debug.DrawRay(transform.position + m_vecFrontDow, transform.forward * m_distFront, Color.green);
+        Debug.DrawRay(transform.position + m_vecFrontUp, transform.forward * m_distFront, Color.green);
+
+        m_direction.y = m_direction.y + (Physics.gravity.y * m_gravityScale * Time.deltaTime);
+        m_controller.Move(m_direction * Time.deltaTime);
+
+
+
+
+    }
+
+
+    void PlayerMove()
+    {
+        //RaycastHit hit;
+        if (!Physics.Raycast(transform.position + m_vecFrontUp, transform.forward, m_distFront) ||
+           !Physics.Raycast(transform.position + m_vecFrontDow, transform.forward, m_distFront))
+        {
+            //if(!m_wallJump)
+            //   m_direction = new Vector3(Input.GetAxis("Horizontal") * m_movSpeed, m_direction.y, 0f);
+            float y = m_direction.y;
+            m_direction = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+            m_direction = m_direction.normalized * m_movSpeed;
+            m_direction.y = y;
+        }
+    }
+
+    void DirLook()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            m_body.transform.eulerAngles = new Vector3(0f, -90f);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            m_body.transform.eulerAngles = new Vector3(0f, 90f);
+        }
+    }
+
+    //private void OnControllerColliderHit(ControllerColliderHit hit)
+    //{
+    //    WallJump(hit);
+    //}
+
+    //void WallJump(ControllerColliderHit hit)
+    //{
+    //        if (!m_controller.isGrounded && hit.normal.y < 0.1f && hit.transform.gameObject.CompareTag("JumpWall"))
+    //        {
+    //            if (Input.GetButtonDown("Jump"))
+    //            {
+    //                m_wallJump = true;
+    //                m_direction = hit.normal * m_jumpWallForce;
+    //                m_direction.y += m_jumpForce;
+    //                if (hit.normal.x > 0f)
+    //                {
+    //                    m_body.transform.eulerAngles = new Vector3(0f, -90f);
+    //                }
+    //                else
+    //                {
+    //                    m_body.transform.eulerAngles = new Vector3(0f, 90f);
+    //                }
+    //            }
+    //        }
+    //        else
+    //        {
+    //             return;
+    //        }
+    //}
+
+}
